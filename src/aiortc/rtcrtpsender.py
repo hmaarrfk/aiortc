ea7d1b5a83e98fd95657292961e851ab342c501a
Mark Harfouche <mark.harfouche@gmail.com>
2025-09-29 09:57:35 -0400

    Disable automatic feedback of target bitrate

--- src/aiortc/rtcrtpsender.py
+++ src/aiortc/rtcrtpsender.py
@@ -139,6 +139,10 @@ class RTCRtpSender:
         return self.__kind
 
     @property
+    def encoder(self):
+        return self.__encoder
+
+    @property
     def track(self) -> MediaStreamTrack:
         """
         The :class:`MediaStreamTrack` which is being handled by the sender.
@@ -282,8 +286,8 @@ class RTCRtpSender:
                     self.__log_debug(
                         "- receiver estimated maximum bitrate %d bps", bitrate
                     )
-                    if self.__encoder and hasattr(self.__encoder, "target_bitrate"):
-                        self.__encoder.target_bitrate = bitrate
+                    # if self.__encoder and hasattr(self.__encoder, "target_bitrate"):
+                    #     self.__encoder.target_bitrate = bitrate
             except ValueError:
                 pass
 
